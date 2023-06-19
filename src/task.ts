a620af098268da391349c2e6778a2050a313c75f
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-06-19 08:28:27 -0500

    adding user and task classes, writing tests

---
+++ src/task.ts
@@ -0,0 +1,15 @@
+class Task {
+    title: string;
+    description: string;
+    status: string;
+    assignee: User;
+    dueDate: Date;
+
+    constructor(title: string, description: string, status: string, assignee: User, dueDate: Date) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.assignee = assignee;
+        this.dueDate = dueDate;
+    }
+}
---
+++ src/user.ts
@@ -0,0 +1,51 @@
+class User {
+    static _nextId: number = 1;
+    id: number;
+    firstName: string;
+    lastName: string;
+    title: string | null;
+    private admin: boolean;
+    private _email: string | null;
+    private _phoneNumber: string | null;
+
+    constructor(
+        firstName: string,
+        lastName: string,
+    ) {
+        this.id = User.nextId();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.admin = false;
+        this._email = null;
+        this._phoneNumber = null;
+        this.title = null;
+    }
+
+    setEmail(email: string) {
+        this._email = email;
+    }
+
+    setPhoneNumber(phoneNumber: string) {
+        this._phoneNumber = phoneNumber;
+    }
+
+    isAdmin(): boolean {
+        return this.admin;
+    }
+
+    private setAdmin(admin: boolean) {
+        this.admin = admin;
+    }
+
+    private static nextId(): number {
+        let id = User._nextId;
+        User._nextId++;
+        return id;
+    }
+
+    toString(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+export = User;
